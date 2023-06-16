bf654fd7102471522f7d190ef3eb6d7083f23fbc
akezeth <116703083+akezeth@users.noreply.github.com>
2023-06-16 18:19:28 +0300

    Add props and states

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,7 +1,45 @@
-const Books = () => {
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import BooksList from './BooksList';
+import InputBook from './InputBook';
+
+function Books() {
+  const [books, setBooks] = useState([
+    {
+      id: uuidv4(),
+      title: 'The Hunger Games',
+      author: 'Suzanne Collins',
+    },
+    {
+      id: uuidv4(),
+      title: 'Dune',
+      author: 'Frank Herbert',
+    },
+    {
+      id: uuidv4(),
+      title: 'Capital in the Twenty-First Century',
+      author: 'Suzanne Collins',
+    },
+  ]);
+  const addBook = (title, author) => {
+    const newBook = {
+      id: uuidv4(),
+      title,
+      author,
+    };
+    setBooks([...books, newBook]);
+  };
+  const delBook = (id) => {
+    setBooks([
+      ...books.filter((book) => book.id !== id),
+    ]);
+  };
   return (
-    <h1>Bookstore</h1>
+    <>
+      <BooksList books={books} delBook={delBook} />
+      <InputBook addBook={addBook} />
+    </>
   );
 }
- 
+
 export default Books;
