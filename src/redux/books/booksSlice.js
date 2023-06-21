55cc1c6af49acd082135122a322da4bfcbfd3a86
akezeth <116703083+akezeth@users.noreply.github.com>
2023-06-21 16:00:15 +0300

    Adjust filter array statement in removebook reducer

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -7,9 +7,7 @@ export const booksSlice = createSlice({
     addBook: (state, action) => {
       state.push(action.payload);
     },
-    removeBook: (state, action) => {
-      state.filter((book) => book.id !== action.payload.id);
-    },
+    removeBook: (state, action) => state.filter((book) => book.id !== action.payload.id),
   },
 });
 
