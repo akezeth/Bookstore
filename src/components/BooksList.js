3779ce3eacde172a9669d6912ed90f62deb76be5
akezeth <116703083+akezeth@users.noreply.github.com>
2023-06-23 20:26:43 +0300

    Update the book list using fetched book data

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,22 +1,69 @@
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/booksSlice';
-import BookItem from './BookItem';
+import { fetchBooks, removeBook } from '../redux/books/booksSlice';
+// import BookItem from "./BookItem";
 
 function BooksList() {
-  const books = useSelector((state) => state.books);
+  const { books, isLoading, error } = useSelector((state) => state.books);
   const dispatch = useDispatch();
 
-  const handleDelete = (id) => {
-    dispatch(removeBook({ id }));
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Could not fetch data.</p>;
+  }
+
+  if (!books) {
+    return null;
+  }
+
+  const renderBook = (itemId, book) => {
+    const {
+      category, title, author, chapter,
+    } = book[0];
+
+    return (
+      <div key={itemId}>
+        <div>
+          <p>{category}</p>
+          <h3>{title}</h3>
+          <p>{author}</p>
+          <div>
+            <button type="button">Comment</button>
+            <div>
+              <button
+                type="button"
+                onClick={() => dispatch(removeBook(itemId))}
+              >
+                Remove
+              </button>
+            </div>
+            <button type="button">Edit</button>
+          </div>
+        </div>
+        <div>
+          <h4>75%</h4>
+          <div>Completed</div>
+        </div>
+        <div>
+          <h3>Current Chapter</h3>
+          <p>{chapter}</p>
+          <button type="button">Update Progress</button>
+        </div>
+      </div>
+    );
   };
+
   return (
-    <>
-      <ul>
-        {books.map((book) => (
-          <BookItem key={book.id} book={book} delBook={handleDelete} />
-        ))}
-      </ul>
-    </>
+    <div>
+      {Object.entries(books).map(([itemId, book]) => renderBook(itemId, book))}
+    </div>
   );
 }
 
