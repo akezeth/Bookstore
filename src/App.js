144a532fce9b8503c5b2b7ae0d5cf7c6495b1c32
akezeth <116703083+akezeth@users.noreply.github.com>
2023-06-24 14:14:20 +0300

    Add missing JSX elements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes, Link } from 'react-router-dom';
+import { UilUser } from '@iconscout/react-unicons';
 import Books from './components/Books';
 import Categories from './components/Categories';
 
@@ -6,25 +7,26 @@ import './App.css';
 
 function App() {
   return (
-    <>
-      <header>
-        <h1>Bookstore CMS</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">BOOKS</Link>
-            </li>
-            <li>
-              <Link to="/categories">CATEGORIES</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+    <div className="topContainer">
+      <div className="navbar">
+        <div className="container">
+          <div className="nav-left">
+            <h2 className="logo">Bookstore CMS</h2>
+            <Link to="/" className="home">
+              Books
+            </Link>
+            <Link to="/categories" className="categories">
+              Categories
+            </Link>
+          </div>
+          <UilUser size="31" color="#0290ff" />
+        </div>
+      </div>
       <Routes>
         <Route path="/" element={<Books />} />
         <Route path="/categories" element={<Categories />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../redux/books/booksSlice';
+import './InputBook.css';
 
 const InputBook = () => {
   const dispatch = useDispatch();
@@ -41,39 +42,42 @@ const InputBook = () => {
 
   return (
     <>
-      <form>
-        <h3>Add a new book...</h3>
-        <input
-          type="text"
-          name="title"
-          placeholder="Title..."
-          value={title}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="author"
-          placeholder="Author..."
-          value={author}
-          onChange={handleInputChange}
-        />
-        <select name="category" value={category} onChange={handleInputChange}>
-          <option value="">Select Category</option>
-          <option value="Memoir">Memoir</option>
-          <option value="Science">Science</option>
-          <option value="Travel">Travel</option>
-          <option value="Business/Finance">Business/Finance</option>
-          <option value="Poetry">Poetry</option>
-          <option value="Science Fiction">Science Fiction</option>
-          <option value="Horror">Horror</option>
-          <option value="Comedy/Humor">Comedy/Humor</option>
-          <option value="Drama/Play">Drama/Play</option>
-          <option value="Novels/Comics">Novels/Comics</option>
-          <option value="Cookbooks">Cookbooks</option>
-        </select>
-        <button type="button" onClick={addBookHandler}>
-          Add Book
-        </button>
+      <form className="form-container">
+        <hr />
+        <h3 className="form-title">ADD NEW BOOK</h3>
+        <div className="form-content">
+          <input
+            type="text"
+            name="title"
+            placeholder="Title..."
+            value={title}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="author"
+            placeholder="Author..."
+            value={author}
+            onChange={handleInputChange}
+          />
+          <select name="category" value={category} onChange={handleInputChange}>
+            <option value="">Select Category</option>
+            <option value="Memoir">Memoir</option>
+            <option value="Science">Science</option>
+            <option value="Travel">Travel</option>
+            <option value="Business/Finance">Business/Finance</option>
+            <option value="Poetry">Poetry</option>
+            <option value="Science Fiction">Science Fiction</option>
+            <option value="Horror">Horror</option>
+            <option value="Comedy/Humor">Comedy/Humor</option>
+            <option value="Drama/Play">Drama/Play</option>
+            <option value="Novels/Comics">Novels/Comics</option>
+            <option value="Cookbooks">Cookbooks</option>
+          </select>
+          <button type="button" onClick={addBookHandler}>
+            ADD BOOK
+          </button>
+        </div>
       </form>
     </>
   );
